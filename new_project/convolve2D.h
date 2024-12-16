ee1cfa94857b9a2767fc5e2594284859d4817830
duclee2004 <127739843+duclee2004@users.noreply.github.com>
2024-12-16 21:48:21 +0700

    Add files via upload

---
+++ new_project/convolve2D.h
@@ -0,0 +1,10 @@
+#ifndef CONVOLVE2D_H
+#define CONVOLVE2D_H
+
+#define ROWS 5
+#define COLS 5
+#define KERNEL_SIZE 3
+
+void convolve2D(float input[ROWS][COLS], float kernel[KERNEL_SIZE][KERNEL_SIZE], float output[ROWS-KERNEL_SIZE+1][COLS-KERNEL_SIZE+1]);
+
+#endif // CONVOLVE2D_H
